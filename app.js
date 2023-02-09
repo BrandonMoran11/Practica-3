fe59d811f718993fd9e4cbc0a6807ef79804a216
20186935 BRANDON ALEXANDER MORAN VAZQUEZ <20186935@TELEMATICA.UCOL.MX>
2023-02-09 08:26:40 -0600

    fly function

---
+++ app.js
@@ -0,0 +1,9 @@
+let logGreeting = (miParametro) => miParametro();
+logGreeting(function () {
+    console.log('Hello from a function created on the fly')
+})
+let logGreeting1 = (miNombre, miColor) => {
+    console.log("Hola " + miNombre + " buenos dias! tu color favorito es el " + miColor);
+}
+
+logGreeting1("Brandon", "Verde");
